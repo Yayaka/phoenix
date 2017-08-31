de2e471d3a1adc3b9ccf7db04c15b8357b0f26f8
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-08-31 23:16:49 +0900

    Use Webpack instead of Brunch

---
+++ apps/web/assets/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+module.exports = {
+  entry: {
+    app: "./js/app",
+  },
+  output: {
+    path: path.join(__dirname, "../priv/static/js"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
--- apps/web/config/dev.exs
+++ apps/web/config/dev.exs
@@ -11,8 +11,7 @@ config :web, Web.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [node: ["node_modules/webpack/bin/webpack.js", "--watch", "--color", cd: Path.expand("../assets", __DIR__)]]
 
 # ## SSL Support
 #
